4683fdf686add5e93419f2f694a40551bc9cec48
juliocabrera820 <juliocabrera820@gmail.com>
2022-04-21 15:08:38 -0500

    change values of user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,11 @@
 FactoryBot.define do
-  factory :user do
-    username { "MyString" }
-    email { "MyString" }
+  factory :user, class: User do
+    username { 'jules' }
+    email { 'jules@gmail.com' }
+  end
+
+  factory :random_user, class: User do
+    username { Faker::Name.female_first_name }
+    email { Faker::Internet.email(domain: 'example') }
   end
 end
