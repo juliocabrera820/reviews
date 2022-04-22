a2c69726eb80b5d456dce1d5f79b6a32ba1b7557
juliocabrera820 <juliocabrera820@gmail.com>
2022-04-21 19:10:22 -0500

    add password attribute

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,10 +2,12 @@ FactoryBot.define do
   factory :user, class: User do
     username { 'jules' }
     email { 'jules@gmail.com' }
+    password { '12345 '}
   end
 
   factory :random_user, class: User do
     username { Faker::Name.female_first_name }
     email { Faker::Internet.email(domain: 'example') }
+    password { Faker::Internet.password(min_length: 8) }
   end
 end
